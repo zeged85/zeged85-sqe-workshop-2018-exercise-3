3b4883578c8b1194f93465d385a32293be6e7f0e
zeged <zeged@yahoo.com>
2018-11-23 18:26:34 +0200

    table added to html

--- src/js/app.js
+++ src/js/app.js
@@ -14,5 +14,40 @@ $(document).ready(function () {
 
 
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+
+        let res = makeTableHTML(str);
+        console.log(res);
+        var pageTable = document.getElementById("parsedTable");
+
+        let i = 0;
+        for (var x in str){
+            i++;
+            var row = pageTable.insertRow(i);
+            let j = 0;
+            for (var y in str[x]){
+                var cell1 = row.insertCell(j);
+                j++;
+                cell1.innerHTML = str[x][y];
+            }
+        }
+       // var row = pageTable.insertRow(i);
+
+        $('#parsedTable').val(res);
+
+
     });
 });
+
+function makeTableHTML(myArray) {
+    var result = "<table border=1>";
+    for(var i=0; i<myArray.length; i++) {
+        result += "<tr>";
+        for(var j=0; j<myArray[i].length; j++){
+            result += "<td>"+myArray[i][j]+"</td>";
+        }
+        result += "</tr>";
+    }
+    result += "</table>";
+
+    return result;
+}
